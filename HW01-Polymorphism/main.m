933027682447343ae26a5433c38817c2df04035f
Langston Duncanson <landun@Langstons-iMac.local>
2017-09-26 14:20:27 -0700

    Initial Commit

---
+++ HW01-Polymorphism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HW01-Polymorphism
+//
+//  Created by Langston Duncanson on 9/26/17.
+//  Copyright © 2017 Langston Duncanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
